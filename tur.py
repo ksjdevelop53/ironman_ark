da58a2a9760b00bcc1d36df49136b72be0f493c0
kangseongjin <rkdd2846@gmail.com>
2020-09-19 17:48:49 +0900

    ark

---
+++ tur.py
@@ -0,0 +1,17 @@
+import turtle as t
+
+
+for i in range(1,100):
+    t.forward(100)
+    t.right(72)
+    t.left(3)
+    t.forward(-45)
+    t.left(38)
+    t.right(100) 
+    t.forward(-30)
+    t.left(20)
+    
+
+
+
+
